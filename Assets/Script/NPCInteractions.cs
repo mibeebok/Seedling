4a85c82c6238e5f23a5efd17487e20df6a31a106
meviymw <mickeymilkxxx@gmail.com>
2025-09-27 16:15:01 +0300

    Update for NPCInteractions and for BearBlink

--- Assets/Script/NPCInteractions.cs
+++ Assets/Script/NPCInteractions.cs
@@ -25,7 +25,7 @@ public class NPCInteraction : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<Rigidbody2D>() != null)
+        if (other.CompareTag("Player"))
         {
             TextE.SetActive(true);
             playerInRange = true;
@@ -34,9 +34,10 @@ public class NPCInteraction : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.GetComponent<Rigidbody2D>() != null)
+        if (other.CompareTag("Player"))
         {
             TextE.SetActive(false);
+            playerInRange = false;
             dialogueManager.EndDialogue(); 
         }
     }
