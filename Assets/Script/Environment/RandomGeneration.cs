f1af60b1ee5295ffea0ccfcc69a4ee0dcefb4145
mibeebok <averkievavera1@gmail.com>
2025-10-04 14:31:44 +0300

    Generating flowers and trees by points

---
+++ Assets/Script/Environment/RandomGeneration.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FlowersGeneration : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+}
